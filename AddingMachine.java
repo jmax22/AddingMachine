9de2ec29973ba19942848bbb3f23d4f7251645b4
jmax22 <49964615+jmax22@users.noreply.github.com>
2020-10-02 18:41:33 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,27 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /* returns the total */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /* adds to the total */
+	  public void add (int value) {
+	  }
+	  /* subtracts from the total */
+	  public void subtract (int value) {
+	  }
+	  /* prints the total */
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+
+}
