e9c832196b82d9d21c6488e191f530f4ccd733b2
jmax22 <49964615+jmax22@users.noreply.github.com>
2020-10-02 20:05:37 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,23 +2,28 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String history = "0";
 	  
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	  }
 	  /* returns the total */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  /* adds to the total */
 	  public void add (int value) {
+		  total = total + value;
+		  history = history + " + " + value;
 	  }
 	  /* subtracts from the total */
 	  public void subtract (int value) {
+		  total = total - value;
+		  history = history + " - " + value;
 	  }
-	  /* prints the total */
+	  /* prints the history total i.e 0 + 4 - 2 + 5 */
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  public void clear() {
